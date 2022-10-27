ba0690e136bf088b2340c940c83f3cba727b8f2c
kellnerj <kellnerj@student.psdr3.org>
2022-10-27 15:57:35 -0500

    Created the mecanum encoder

--- TeamCode/src/main/java/encoders/MecanumEncoder.java
+++ TeamCode/src/main/java/encoders/MecanumEncoder.java
@@ -1,4 +1,63 @@
 package encoders;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class MecanumEncoder {
+    LinearOpMode linearOp;
+    DcMotor motorFrontLeft;
+    DcMotor motorBackLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackRight;
+
+    public MecanumEncoder(LinearOpMode linearOp) {
+        this.linearOp = linearOp;
+        HardwareMap hardwareMap = linearOp.hardwareMap;
+
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    public void moveForward(float inches) throws InterruptedException {
+        float time = 0.5f*inches;
+
+        setPower(1, 1, 1, 1);
+
+        linearOp.wait((long)(1000 * time));
+
+        setPower(0,0,0,0);
+    }
+
+    public void rotateDegrees(float degrees) throws InterruptedException {
+        float time = Math.abs(degrees)/90;
+
+        if (degrees > 0) {
+            setPower(1, -1, -1, 1);
+        } else {
+            setPower(-1, 1, 1, -1);
+        }
+
+        linearOp.wait((long)(1000 * time));
+
+        setPower(0,0,0,0);
+    }
+
+    public void setPower(float frontLeft, float backLeft, float frontRight, float backRight) {
+        motorFrontLeft.setPower(frontLeft);
+        motorBackLeft.setPower(backLeft);
+        motorFrontRight.setPower(frontRight);
+        motorBackRight.setPower(backRight);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import encoders.MecanumEncoder;
+
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
     @Override
@@ -15,16 +17,6 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-
-        motorFrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // Reverse the right side motors
-        // Reverse left motors if you are using NeveRests
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
@@ -38,7 +30,6 @@ public class MecanumTeleOp extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            // yes
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
