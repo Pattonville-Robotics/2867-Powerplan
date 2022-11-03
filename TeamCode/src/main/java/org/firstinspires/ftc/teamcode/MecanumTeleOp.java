c8117f4e2c7ddef328226f96d42a1c98c7a1eef1
Zach <zacharyzarosa@gmail.com>
2022-11-03 15:49:57 -0500

    removed what is now the mecanum encoder; added more gamepad vars for later use

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -6,40 +6,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import encoders.MecanumEncoder;
-
+import encoders.LinearSlideEncoder;
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double lx = gamepad1.left_stick_x * 1.1;    // Counteract imperfect strafing
+            double ly = -gamepad1.left_stick_y;         // Remember, this is reversed!
+            boolean lc = gamepad1.left_stick_button;
+
             double rx = gamepad1.right_stick_x;
+            double ry = gamepad1.right_stick_y;         // Not necessarily necessary.
+            boolean rc = gamepad1.right_stick_button;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            boolean dpU = gamepad1.dpad_up;
+            boolean dpD = gamepad1.dpad_down;
 
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
+            boolean a = gamepad1.a;
+            boolean b = gamepad1.b;
+
+
+
         }
     }
 }
