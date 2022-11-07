c0126ac7e2c2e25055a4b2ff175b8013235e9e2c
Zach <zacharyzarosa@gmail.com>
2022-11-07 15:38:46 -0600

    added mavenCentral; more to gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.encoders.MecanumEncoder;
 
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
     MecanumEncoder encoder;
-
+    
     @Override
     public void runOpMode() throws InterruptedException {
         MecanumEncoder encoder = new MecanumEncoder(this);
@@ -21,7 +22,6 @@ public class MecanumTeleOp extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
-
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
@@ -32,21 +32,21 @@ public class MecanumTeleOp extends LinearOpMode {
             double backRightPower = (y + x - rx) / denominator;
 
             encoder.setPower(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
-//            boolean dpU = gamepad1.dpad_up;
-//            boolean dpD = gamepad1.dpad_down;
-//            boolean dpL = gamepad1.dpad_left;
-//            boolean dpR = gamepad1.dpad_right;
+//          boolean dpU = gamepad1.dpad_up;
+//          boolean dpD = gamepad1.dpad_down;
+//          boolean dpL = gamepad1.dpad_left;
+//          boolean dpR = gamepad1.dpad_right;
 //
 //
-//            if (dpU) {
-//                encoder.moveForward(6);
-//            } else if (dpD) {
-//                encoder.moveForward(-6) {
-//            } else if (dpR) {
-//                encoder.rotateDegrees(5);
-//            } else if (dpL) {
-//                encoder.rotateDegrees(-5);
-//            }
+//          if (dpU) {
+//              encoder.moveForward(6);
+//          } else if (dpD) {
+//              encoder.moveForward(-6) {
+//          } else if (dpR) {
+//              encoder.rotateDegrees(5);
+//          } else if (dpL) {
+//              encoder.rotateDegrees(-5);
+//          }
         }
     }
 }
