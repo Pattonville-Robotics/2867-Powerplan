d3435876e1281b27ba0438b174efbcd00efd4d82
Zach <zacharyzarosa@gmail.com>
2022-11-11 21:51:14 -0600

    Added linear slide vertical control to MecanumTeleOp.java (hopefully) and removed unnecessary second LinearSLideEncoder.java file.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -1,52 +1,66 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
+import org.firstinspires.ftc.teamcode.encoders.LinearSlideEncoder;
 import org.firstinspires.ftc.teamcode.encoders.MecanumEncoder;
 
 @TeleOp
 public class MecanumTeleOp extends LinearOpMode {
-    MecanumEncoder encoder;
-    
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumEncoder encoder = new MecanumEncoder(this);
-        float speed = 0;
-        waitForStart();
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+        final LinearSlideEncoder sEncoder = new LinearSlideEncoder(this);
+        final MecanumEncoder mEncoder = new MecanumEncoder(this);
 
+        waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
+            double y = -gamepad.getLeftY(); // Remember, this is reversed!
+            double x = gamepad.getLeftX() * 1.1; // Counteract imperfect strafing
+            double rx = gamepad.getRightX();
+            /*
+            Denominator is the largest motor power (absolute value) or 1
+            This ensures all the powers maintain the same ratio, but only when
+            at least one is out of the range [-1, 1]
+            */
+            // Mecanum movement
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
+            mEncoder.setPower(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
 
-            encoder.setPower(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
-//          boolean dpU = gamepad1.dpad_up;
-//          boolean dpD = gamepad1.dpad_down;
-//          boolean dpL = gamepad1.dpad_left;
-//          boolean dpR = gamepad1.dpad_right;
-//
-//
-//          if (dpU) {
-//              encoder.moveForward(6);
-//          } else if (dpD) {
-//              encoder.moveForward(-6) {
-//          } else if (dpR) {
-//              encoder.rotateDegrees(5);
-//          } else if (dpL) {
-//              encoder.rotateDegrees(-5);
-//          }
+            // Linear slide speed
+            ButtonReader rB = new ButtonReader(gamepad, GamepadKeys.Button.RIGHT_BUMPER);
+            ButtonReader lB = new ButtonReader(gamepad, GamepadKeys.Button.LEFT_BUMPER);
+            float slideSpeed = (float) 1;
+            if (lB.wasJustPressed() && slideSpeed > 0.05) {
+                slideSpeed -= 0.05;
+            }
+            if (rB.wasJustPressed() && slideSpeed < 2) {
+                slideSpeed += 0.05;
+            }
+            // Linear slide movement
+            GamepadButton dpU = new GamepadButton(gamepad, GamepadKeys.Button.DPAD_UP);
+            GamepadButton dpD = new GamepadButton(gamepad, GamepadKeys.Button.DPAD_DOWN);
+            if (dpU.get()) {
+                sEncoder.changeHeight(1, slideSpeed);
+            }
+            if (dpD.get()) {
+                sEncoder.changeHeight(-1, slideSpeed);
+            }
+
+            // Claw
+            ButtonReader a = new ButtonReader(gamepad, GamepadKeys.Button.A);
+
         }
     }
 }
