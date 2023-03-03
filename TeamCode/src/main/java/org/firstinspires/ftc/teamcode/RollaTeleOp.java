5667870e40584a9e0b7db03e8c25e78050a1cb38
three-halves <threehalves1@gmail.com>
2023-03-03 11:49:31 -0600

    added driver-centric driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RollaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RollaTeleOp.java
@@ -4,13 +4,17 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.encoders.ClawEncoder;
 import org.firstinspires.ftc.teamcode.encoders.BarEncoder;
 import org.firstinspires.ftc.teamcode.encoders.LinearSlideEncoder;
@@ -19,10 +23,10 @@ import org.firstinspires.ftc.teamcode.encoders.OmniEncoder;
 @TeleOp
 public class RollaTeleOp extends LinearOpMode {
 
-    // constants/declarations go here
-
     // how fast the V4b motor will move with analogue stick
     private final double armSpd = 10;
+
+    double heading;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -32,6 +36,13 @@ public class RollaTeleOp extends LinearOpMode {
         final OmniEncoder drive = new OmniEncoder(this);
         final LinearSlideEncoder slide = new LinearSlideEncoder(this);
         final ClawEncoder claw = new ClawEncoder(this);
+
+        final IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
 
         waitForStart();
         if (isStopRequested()) return;
@@ -44,10 +55,15 @@ public class RollaTeleOp extends LinearOpMode {
             double y = gamepad1.left_stick_y;
             double rx = gamepad1.right_stick_x;
 
-            double topP = (-x + rx);
-            double bottomP = (x + rx);
-            double leftP = (y + rx);
-            double rightP = (-y + rx);
+            // driver centric stuffs
+            heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double driverX = x * Math.cos(-heading) - y * Math.sin(-heading);
+            double driverY = x * Math.sin(-heading) + y * Math.cos(-heading);
+
+            double topP = (-driverX + rx);
+            double bottomP = (driverX + rx);
+            double leftP = (driverY + rx);
+            double rightP = (-driverY + rx);
 
             drive.setPower(topP, bottomP, leftP, rightP);
 
@@ -68,7 +84,6 @@ public class RollaTeleOp extends LinearOpMode {
                     claw.openClaw();
                 }
             }
-
 
             // telem
             telemetry.addData("topP ", topP);
